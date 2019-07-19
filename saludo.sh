a98f47d07364def9d7bc343aaaef99ef7e197442
Dro-Rodrix <juan1420125.x@gmail.com>
2019-07-19 11:27:03 -0500

    Ejercicio git 1

---
+++ saludo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Echo por Dro-Rodrix
+
+echo "Hola!"
+
+echo "INtroduce tu nombre: "
+read nombre
+echo "Digite tu edad: "
+read edad
+echo "Hola eres" $nombre "y tienes" $edad "años."
+sleep 4
+ echo "Practica de Dro-Rodrix"
+
